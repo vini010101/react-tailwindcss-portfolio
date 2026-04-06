4698dbbf1133337d194368dfe0cc856c42b2da2e
vini010101 <viniciuscalebebarbosa@gmail.com>
2026-04-06 20:08:47 -0300

    Update

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -2,9 +2,6 @@ import { useState } from 'react';
 import { FiMenu, FiMoon, FiSun, FiX } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import useThemeSwitcher from '../../hooks/useThemeSwitcher';
-import HireMeModal from '../HireMeModal';
-import logoLight from '../../images/logo-light.svg';
-import logoDark from '../../images/logo-dark.svg';
 import { motion } from 'framer-motion';
 import Button from '../reusable/Button';
 
