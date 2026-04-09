9b2c6553c5195f1774d7318f6d224ee145d87f98
vini010101 <viniciuscalebebarbosa@gmail.com>
2026-04-09 00:02:18 -0300

    Update: retirei todos os links que não tinham nada aver com o meu portfolio

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -4,7 +4,7 @@ function AppFooterCopyright() {
 			<div className="text-lg text-ternary-dark dark:text-ternary-light">
 				&copy; {new Date().getFullYear()}
 				<a
-					href="https://github.com/realstoman/react-tailwindcss-portfolio"
+					
 					target="__blank"
 					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
@@ -12,7 +12,7 @@ function AppFooterCopyright() {
 				</a>
 				.
 				<a
-					href="https://stoman.me"
+					href="https://www.linkedin.com/in/vinicius-moura-5700b6240/"
 					target="__blank"
 					className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
